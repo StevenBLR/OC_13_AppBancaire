81625309fd08fa0ba6172c5ff200e2bfc2632574
StevenBLR_PC <s.belair95@gmail.com>
2022-01-19 15:19:26 +0100

    Raw integration Signin + User pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+//import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SignIn from "./pages/SignIn";
+import User from "./pages/User";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Header />
+      {/* <Home/>
+      <SignIn/> */}
+      <User />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import App from "./App";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SignIn from "./pages/SignIn";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}>
+          <SignIn />
+        </Route>
+      </Routes>
+
+      <Footer />
+    </Router> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
