d23659b94676e3bd7840d10ee51edfae97a8c749
StevenBLR_PC <s.belair95@gmail.com>
2022-01-26 16:43:47 +0100

    Setting up functions for localstorage

---
+++ src/data/localStorage.js
@@ -0,0 +1,17 @@
+/**
+ * Store data in local storage
+ * @param {String} label Data label
+ * @param {Object} data  Data
+ */
+export function setData(label, data) {
+  localStorage.setItem(label, JSON.stringify(data));
+}
+
+/**
+ * Get data from local storage
+ * @param {String} label Data label
+ * @returns Promise containing stored item
+ */
+export function getData(label) {
+  return localStorage.getItem(label);
+}
