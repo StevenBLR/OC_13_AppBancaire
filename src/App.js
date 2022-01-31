39085ba1184f0deea53d0188d8e87e07170c7a3c
StevenBLR_PC <s.belair95@gmail.com>
2022-01-31 14:17:36 +0100

    React hook form integrated (Signin page)

--- src/App.js
+++ src/App.js
@@ -5,27 +5,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SignIn from "./pages/SignIn";
 import User from "./pages/User";
-import { store } from "./data/redux";
-import { Provider, useDispatch } from "react-redux";
 
 function App() {
-  useEffect(() => {
-    // Get redux state
-    const state = store.getState();
-    console.log(state);
-  }, []);
-
   return (
     <Router>
-      <Provider store={store}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/user" element={<User />}></Route>
-        </Routes>
-        <Footer />
-      </Provider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/user" element={<User />}></Route>
+      </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,48 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form"; // hook permettant de recuperer facilement le contenu d'un formulaire
 import { useSelector } from "react-redux"; // hook react-redux permettant de lire le state redux
 import { useDispatch } from "react-redux"; // hook react-redux permettant de lancer une action (run action)
 import { useNavigate } from "react-router";
 
 function SignIn() {
-  //if (user) navigation.navigate("User"); // redirect vers par user si deja connecté
   const navigation = useNavigate();
-  const dispatch = useDispatch(); // On utilise le hook useDispatch dans notre composant
-  const userLogged = useSelector((state) => state.loggedIn); // On stock l'état de notre substate "logged in"
+  //const dispatch = useDispatch(); // On utilise le hook useDispatch dans notre composant
+  //const userLogged = useSelector((state) => state.loggedIn); // On stock l'état de notre substate "logged in"
+  const { register, handleSubmit } = useForm();
 
-  // X - If user is logged already --> Redirect to user page
+  // Login with user credentials
+  // const handleSubmit = () => {
+  //   // On excecute la fonction dispatch avec une action
+  //   dispatch({ type: "login" });
+  // };
 
-  const handleSubmit = () => {
-    // On excecute la fonction dispatch avec une action
-    dispatch({ type: "login" });
-  };
+  // Redirect user to previous page if he is already logged in
+  // useEffect(() => {
+  //   if (!userLogged) {
+  //     navigation(-1); // Go back to previous page
+  //   }
+  // }, [userLogged, navigation]);
 
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
           <div className="input-wrapper">
             <label for="username">Username</label>
-            <input type="text" id="username" />
+            <input {...register("userName")} type="text" id="username" />
           </div>
           <div className="input-wrapper">
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input {...register("password")} type="password" id="password" />
           </div>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
+            <input
+              {...register("remember-me")}
+              type="checkbox"
+              id="remember-me"
+            />
             <label for="remember-me">Remember me</label>
           </div>
           {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
 
           {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
           {/* Utiliser react hook form pour recuprerer les values dui form */}
-          <button
-            type="submit"
-            className="sign-in-button"
-            onClick={() => handleSubmit()}
-          >
+          <button type="submit" className="sign-in-button">
             Sign In
           </button>
         </form>
